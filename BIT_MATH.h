39eeef815486651289ba5239160267000b267cc9
osalem192 <124590406+osalem192@users.noreply.github.com>
2023-02-06 17:16:29 +0200

    Add files via upload
    Task 1

---
+++ BIT_MATH.h
@@ -0,0 +1,17 @@
+/*
+ * BIT_MATH.h
+ *
+ *  Author: omar salem
+ */ 
+
+
+#ifndef BIT_MATH_H_
+#define BIT_MATH_H_
+
+#define reg 8
+#define SetBit(x,y) x|=(1<<y)
+#define ClrBit(x,y) x&=(~(1<<y))
+#define ToggleBit(x,y) x^=(1<<y)
+#define ReadBit(x,y) ((x&(1<<y))>>y))
+
+#endif /* BIT_MATH_H_ */
